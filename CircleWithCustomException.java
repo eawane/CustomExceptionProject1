493ff8f164c51c8e1c26e942f18d2b8a65a9282e
eawane <eawane@ivytech.edu>
2026-03-30 19:07:48 -0400

    Create CircleWithCustomException.java

---
+++ CircleWithCustomException.java
@@ -0,0 +1,29 @@
+public class CircleWithCustomException {
+    private double radius;
+    private static int numberOfObjects = 0;
+
+    public CircleWithCustomException() throws InvalidRadiusException {
+        this(1.0);
+    }
+
+    public CircleWithCustomException(double newRadius) throws InvalidRadiusException {
+        setRadius(newRadius);
+        numberOfObjects++;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double newRadius) throws InvalidRadiusException {
+        if (newRadius >= 0) {
+            radius = newRadius;
+        } else {
+            throw new InvalidRadiusException(newRadius);
+        }
+    }
+
+    public static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+}
